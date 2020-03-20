951e96457efa09cbe6d4f70ccbd45cb2798c2da8
cledegario <cassio.leodegario@gmail.com>
2020-03-20 15:31:28 -0400

    saga + finishing

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMonitor = process.env.NODE_ENV == 'development'
+const sagaMonitor = process.env.NODE_ENV === 'development'
   ? console.tron.createSagaMonitor()
   : null;
 
